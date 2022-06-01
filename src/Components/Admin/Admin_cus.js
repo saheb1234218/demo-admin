2469d06a20b1bb8558e98049347a14a19193ec48
saheb1234218 <sahebganguly1234@gmail.com>
2022-06-01 23:42:57 +0530

    navbar done'

--- src/Components/Admin/Admin_cus.js
+++ src/Components/Admin/Admin_cus.js
@@ -3,99 +3,99 @@ import Admin_main from './Admin_main'
 export default function Admin_cus() {
   return (
     <div className='container  my-4 p-4'>
-        <table class="table  table-striped table-hover shadow">
-        <thead style={{backgroundColor:'#ff8533'}}>
-    <tr >
-      <th scope="col">ID</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Phone</th>
-      <th scope="col">Housekeeper Booked</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1234</th>
-      
-      <td>Apurba Ganguly</td>
-      <td>xyz@gmail.com</td>
-      <td>1234567890</td>
-      <td>Abdul Siddique</td>
-    </tr>
-    <tr>
-    <th scope="row">1234</th>
-      
-      <td>Apurba Ganguly</td>
-      <td>xyz@gmail.com</td>
-      <td>1234567890</td>
-      <td>Abdul Siddique</td>
-    </tr>
-    <tr>
-    <th scope="row">1234</th>
-      
-      <td>Apurba Ganguly</td>
-      <td>xyz@gmail.com</td>
-      <td>1234567890</td>
-      <td>Abdul Siddique</td>
-    </tr>
-    <tr>
-    <th scope="row">1234</th>
-      
-      <td>Apurba Ganguly</td>
-      <td>xyz@gmail.com</td>
-      <td>1234567890</td>
-      <td>Abdul Siddique</td>
-    </tr>
-    <tr>
-    <th scope="row">1234</th>
-      
-      <td>Apurba Ganguly</td>
-      <td>xyz@gmail.com</td>
-      <td>1234567890</td>
-      <td>Abdul Siddique</td>
-    </tr>
-    <tr>
-    <th scope="row">1234</th>
-      
-      <td>Apurba Ganguly</td>
-      <td>xyz@gmail.com</td>
-      <td>1234567890</td>
-      <td>Abdul Siddique</td>
-    </tr>
-    <tr>
-    <th scope="row">1234</th>
-      
-      <td>Apurba Ganguly</td>
-      <td>xyz@gmail.com</td>
-      <td>1234567890</td>
-      <td>Abdul Siddique</td>
-    </tr>
-    <tr>
-    <th scope="row">1234</th>
-      
-      <td>Apurba Ganguly</td>
-      <td>xyz@gmail.com</td>
-      <td>1234567890</td>
-      <td>Abdul Siddique</td>
-    </tr>
-  </tbody>
-</table>
+      <table class="table bg-table  table-striped table-responsive table-hover shadow">
+        <thead style={{ backgroundColor: '#ff8533' }}>
+          <tr >
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Email</th>
+            <th scope="col">Phone</th>
+            <th scope="col">Housekeeper Booked</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1234</th>
 
-<nav aria-label="..." className='my-5 d-flex justify-content-center'>
-  <ul class="pagination">
-    <li class="page-item disabled">
-      <a class="page-link">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item active" aria-current="page">
-      <a class="page-link" href="#">2</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
-</nav>
+            <td>Apurba Ganguly</td>
+            <td>xyz@gmail.com</td>
+            <td>1234567890</td>
+            <td>Abdul Siddique</td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>xyz@gmail.com</td>
+            <td>1234567890</td>
+            <td>Abdul Siddique</td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>xyz@gmail.com</td>
+            <td>1234567890</td>
+            <td>Abdul Siddique</td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>xyz@gmail.com</td>
+            <td>1234567890</td>
+            <td>Abdul Siddique</td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>xyz@gmail.com</td>
+            <td>1234567890</td>
+            <td>Abdul Siddique</td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>xyz@gmail.com</td>
+            <td>1234567890</td>
+            <td>Abdul Siddique</td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>xyz@gmail.com</td>
+            <td>1234567890</td>
+            <td>Abdul Siddique</td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>xyz@gmail.com</td>
+            <td>1234567890</td>
+            <td>Abdul Siddique</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <nav aria-label="..." className='my-5 d-flex justify-content-center'>
+        <ul class="pagination">
+          <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active" aria-current="page">
+            <a class="page-link" href="#">2</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
--- src/Components/Admin/Admin_main.js
+++ src/Components/Admin/Admin_main.js
@@ -1,65 +1,66 @@
 import React from 'react'
 import Admin_cus from './Admin_cus'
-
+import App_icon from "../../images/App_logo.jpeg";
+import './admin.css'
 export default function Admin_main() {
   return (
     <div>
-        <nav class="navbar bg-topbar text-light">
-  <div class="container-fluid container-fluid-sm">
-  <ul class="flex-row navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav mx-3">
-          <button className='bi bi-justify border-none' type="button"   data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"></button>
-       </li>
-        <li class="nav mx-3">
-        <img src="https://th.bing.com/th/id/OIP.vR3c8gJDtTZuFFJLa3nHcwHaHC?w=183&h=180&c=7&r=0&o=5&pid=1.7" alt="" width="30" height="24" class="d-inline-block align-text-top"/>
-        </li>
-        <li className='nav  mx-3'>
-        <h4>Manage My House</h4>
-        </li>
-        
-    </ul>
-    <span class="navbar-text">
-        Hello Admin
-        
-      </span> 
-    
-      
-  </div>
-</nav>
+      <nav class="navbar bg-topbar text-light">
+        <div class="container-fluid container-fluid-sm">
+          <ul class="flex-row navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav mx-2">
+              <i className='bi-justify' style={{fontSize:'30px'}} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"></i>
+            </li>
+            <li class="nav mx-1">
+              <img src={App_icon} alt="" width="60" height="40" class="d-inline-block align-text-top" />
+            </li>
+            <li className='nav text-center my-1  mx-0'>
+              <h4>Manage My House</h4>
+            </li>
+
+          </ul>
+          <span className="navbar-text mx-2 ">
+            Hello Admin
+            
+          </span>
+          <img className="admin_image" src="https://www.bing.com/th?id=OIP.p3GWq9o6cGrlzg8DrxS_EAHaK3&w=206&h=302&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"></img>
+
+        </div>
+      </nav>
 
 
-<div class="offcanvas  offcanvas-sm offcanvas-start bg-sidebar text-light" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
-  <div class="offcanvas-header">
-    <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Manage My House</h5>
-    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body my-5">
-    
-  <ul class="nav flex-column text-light hover gap-3">
-  <li class="nav-item nav-item-hover">
-    <a class="nav-link text-light"  href="#"><h4>Dashboard</h4></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link text-light"  href="#"><h4>Customers</h4></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link text-light"  href="#"><h4>Housekeepers</h4></a>
-  </li>
+      <div class="offcanvas  offcanvas-sm offcanvas-start bg-sidebar text-light" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Manage My House</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body my-5">
 
-</ul>
-<ul class="nav my-4  flex-column text-light hover gap-3">
-  <li class="nav-item">
-    <a class="nav-link text-light"  href="#"><h4>Profile</h4></a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link text-light"  href="#"><h4>Logout</h4></a>
-  </li>
-  
-</ul>
-  </div>
-</div>
-<Admin_cus/>
+          <ul class="nav flex-column text-light hover gap-3">
+            <li class="nav-item nav-item-hover">
+              <a class="nav-link text-light" href="#"><h4>Dashboard</h4></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#"><h4>Customers</h4></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#"><h4>Housekeepers</h4></a>
+            </li>
+
+          </ul>
+          <ul class="nav my-4  flex-column text-light hover gap-3">
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#"><h4>Profile</h4></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#"><h4>Logout</h4></a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+      <Admin_cus />
     </div>
   )
 
-  }
+}
