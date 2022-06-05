c0c94f7818379b0ecfffe5983f3ff63a27bfc193
saheb1234218 <sahebganguly1234@gmail.com>
2022-06-05 17:39:33 +0530

    sddfdfg

---
+++ src/Components/Admin/Admin_main.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Admin_cus from './Admin_tickets'
+import App_icon from "../../images/App_logo.jpeg";
+import './admin.css'
+export default function Admin_main() {
+  return (
+    <div>
+      <nav class="navbar bg-topbar text-light">
+        <div class="container-fluid container-fluid-sm">
+          <ul class="flex-row navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav mx-2">
+              <i className='bi-justify' style={{ fontSize: '30px' }} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBackdrop" aria-controls="offcanvasWithBackdrop"></i>
+
+            </li>
+            <li class="nav mx-1">
+              <img src={App_icon} alt="" width="60" height="40" class="d-inline-block align-text-top" />
+            </li>
+            <li className='nav  text-center my-1  mx-0'>
+              <h4 className="app_text">Manage My House</h4>
+            </li>
+
+          </ul>
+          <span className="navbar-text mx-2 ">
+            Hello Admin
+
+          </span>
+          <img className="admin_image" src="https://www.bing.com/th?id=OIP.p3GWq9o6cGrlzg8DrxS_EAHaK3&w=206&h=302&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"></img>
+
+        </div>
+      </nav>
+
+
+      <div class="offcanvas offcanvas-start bg-sidebar" tabindex="-1" id="offcanvasWithBackdrop" aria-labelledby="offcanvasWithBackdropLabel">
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title text-light" id="offcanvasWithBackdropLabel">Manage My House</h5>
+          <img src={App_icon} alt="" width="60" height="40" class="d-inline-block align-text-top" />
+          <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body my-5">
+          <ul class="nav flex-column text-light hover gap-3">
+            <li class="nav-item nav-item-hover">
+              <a class="nav-link text-light" href="#"><h4>Dashboard</h4></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#"><h4>Customers</h4></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#"><h4>Housekeepers</h4></a>
+            </li>
+
+          </ul>
+          <ul class="nav my-4  flex-column text-light hover gap-3">
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#"><h4>Profile</h4></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link text-light" href="#"><h4>Logout</h4></a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+      <Admin_cus />
+    </div>
+  )
+
+}
---
+++ src/Components/Admin/Admin_tickets.js
@@ -0,0 +1,102 @@
+import React from 'react'
+
+export default function Admin_tickets() {
+  return (
+    <div className='container  my-4 p-4'>
+      
+      <table class="table bg-table   table-responsive table-hover ">
+        <thead style={{ backgroundColor: '#ff8533' }}>
+          <tr >
+            <th scope="col">Ticket ID</th>
+            <th scope="col">Issuer Name</th>
+            <th scope="col">Issue Date</th>
+            <th scope="col">Status</th>
+            <th scope="col">Click to Solve</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>21/4/22</td>
+            <td>Solved</td>
+            <td><button type="button" class="w-50 btn btn-green bg-green text-light">Resolved</button></td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>21/4/22</td>
+            <td>Pending</td>
+            <td><button type="button" class="w-50 btn btn-danger bg-danger text-light">Solve</button></td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>21/4/22</td>
+            <td>Solved</td>
+            <td><button type="button" class="w-50 btn btn-green bg-green text-light">Resolved</button></td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>21/4/22</td>
+            <td>Pending</td>
+            <td><button type="button" class="w-50 btn btn-danger bg-danger text-light">Solved</button></td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>21/4/22</td>
+            <td>Solved</td>
+            <td><button type="button" class="w-50 btn btn-danger bg-danger text-light">Resolved</button></td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>21/4/22</td>
+            <td>Solved</td>
+            <td><button type="button" class="w-50 btn btn-danger bg-danger text-light">Resolved</button></td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>21/4/22</td>
+            <td>Solved</td>
+            <td><button type="button" class="w-50 btn btn-danger bg-danger text-light">Resolved</button></td>
+          </tr>
+          <tr>
+            <th scope="row">1234</th>
+
+            <td>Apurba Ganguly</td>
+            <td>21/4/22</td>
+            <td>Solved</td>
+            <td><button type="button" class="w-50 btn btn-danger bg-danger text-light">Resolved</button></td>
+          </tr>
+        </tbody>
+      </table>
+
+      <nav aria-label="..." className='my-5 d-flex justify-content-center'>
+        <ul class="pagination">
+          <li class="page-item disabled">
+            <a class="page-link">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active" aria-current="page">
+            <a class="page-link" href="#">2</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+    )
+}
