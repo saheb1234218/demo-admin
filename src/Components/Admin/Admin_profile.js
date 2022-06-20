40ee3fca2a05a3209c8e90c646b5952237891d17
saheb1234218 <sahebganguly1234@gmail.com>
2022-06-20 09:05:38 +0530

    added edit profile

--- src/Components/Admin/Admin_profile.js
+++ src/Components/Admin/Admin_profile.js
@@ -1,41 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Nav from "./Admin_main";
 import "./admin.css"
+
 export default function Admin_profile() {
+  const [makedisable,setMakedisable]=useState(true);
   return (
     <>
     <Nav/>
-    <div className="container my-5">
-    <form>
-  <div class="form-group d-flex flex-row justify-content-evenly">
-  <img className="adminimage" width="180px" height="190px" src="https://www.bing.com/th?id=OIP.p3GWq9o6cGrlzg8DrxS_EAHaK3&w=206&h=302&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"></img>
-    <ul>
-        <li className='d-flex flex-row justify-content-between'>
-            <h3>Name:</h3>
-            <h4>Admin User</h4>
-        </li>
-    </ul>
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-check d-flex flex-row gap-5 mt-5 mb-4">
-  <label for="exampleInputPassword1">Address</label>
-    <input disabled="true" type="password" style={{boxShadow:'1px 1px 4px 1px orange'}}  class="form-control" id="exampleInputPassword1" placeholder="ramanath Sarani Sb Gorai Road"/>
-  </div>
-  <div class="form-check d-flex flex-row gap-5 my-4">
-  <label for="exampleInputPassword1">Phone</label>
-    <input disabled="true" type="password" style={{boxShadow:'1px 1px 4px 1px orange'}}  class="form-control" id="exampleInputPassword1" placeholder="7001365857"/>
-  </div>
-  <div class="form-check  d-flex flex-row gap-5 my-4">
-  <label for="exampleInputPassword1" class='font-weight-bold' >Email</label>
-    <input disabled="true" type="password" style={{boxShadow:'1px 1px 4px 1px orange'}}  className="form-control" id="exampleInputPassword1" placeholder="sahbeganguly1234@gmail.com"/>
-  </div>
-  <div class="form-check  d-flex flex-row gap-5 my-4">
-  <label for="exampleInputPassword1" class='font-weight-bold' >Gender</label>
-    <input disabled="true" type="password" style={{boxShadow:'1px 1px 4px 1px orange'}}  className="form-control" id="exampleInputPassword1" placeholder="Male"/>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
-    </div>
+    <div class="container bg-primary my-5 p-5">
+        <div class="card mb-3" >
+            <div class="row no-gutters">
+              <div class="col-md-4 ">
+                  <img src="https://www.bing.com/th?id=OIP.p3GWq9o6cGrlzg8DrxS_EAHaK3&w=206&h=302&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2" height="90%" class="card-img" alt="..."></img>
+              </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <div className='d-flex flex-col justify-content-between'>
+                <h1 class="card-title">Admin User</h1>
+                <button type="button" class="h-75 btn btn-danger bg-primary text-light" onClick={()=>setMakedisable(false)}>{makedisable? <>Edit</>:<>Save</>}</button>
+                </div>
+                  
+                  <hr/>
+                 
+                  <ul >
+                  <div>
+                     <li className='d-flex flex-row justify-content-between m-2'>
+                       <h5>Name:</h5>
+                       <input disabled={makedisable} type="text" className="form-control m-2" id="hkName" placeholder="Admin User"/>
+                    </li>
+                    <li className='d-flex flex-row justify-content-between m-3'>
+                       <h5>Phone:</h5>
+                       <input disabled={makedisable} type="text" className="form-control m-2" id="bookId" placeholder="7001365857"/>
+                    </li>
+                    <li className='d-flex flex-row justify-content-between  m-3'>
+                       <h5>Email:</h5>
+                       <input disabled={makedisable} type="email"  className="form-control m-2" id="bookDate" placeholder="xyz@gmail.com"/>
+
+                    </li>
+                    <li className='d-flex flex-row justify-content-between  m-3'>
+                       <h5>Address:</h5>
+                       <input disabled={makedisable} type="text"  className="form-control m-2" id="cusName" placeholder="Ramanath sarani sb gorai road"/>
+                    </li>
+                    </div>
+                    <hr/>
+                    
+                  <div class="my-4">
+                    <li className='d-flex flex-row justify-content-between  m-3'>
+                       <h5>Role:</h5>
+                       <input disabled="true" type="text"  className="form-control m-2" id="salary" placeholder="Admin"/>
+
+                    </li>
+                    
+                  </div>
+                
+                  </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
